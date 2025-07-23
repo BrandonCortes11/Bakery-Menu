d3034f3c4f539038155d89a42bb72469078f7636
Brandon Cortes <brandonco547@gmail.com>
2025-07-23 01:40:45 -0700

    Buttons are now visible and added background image

--- Level 1/Mini-HTML-JS-Projects/Bakery Menu Builder/script.js
+++ Level 1/Mini-HTML-JS-Projects/Bakery Menu Builder/script.js
@@ -1,83 +1,22 @@
 const dessert = [
-    {
-        name: IceCream,
-        price: 3.75,
-        inStock: true
-    },
-    {
-        name: FruitRoll,
-        price: 1.50,
-        inStock: false
-    },
-    {
-        name: CinnamonBun,
-        price: 2.25,
-        inStock: true
-    },
-    {
-        name: Cupcake,
-        price: 1.75,
-        inStock: false
-    }
-]
+    {name: "IceCream", price: 3.75, inStock: true },
+    {name: "FruitRoll", price: 1.5, inStock: false },
+    {name: "CinnamonBun", price: 2.25, inStock: true },
+    {name: "CupCake", price: 1.75, inStock: false },
+];
 
 const menuButtonsContainer = document.getElementById("menu-buttons");
 const orderSummaryContainer = document.getElementById("order-summary");
 
-dessert.forEach(dessert => {
+dessert.forEach((dessertItem) => {
     const button = document.createElement("button");
-    button.textContent = dessert.IceCream;
+    button.textContent = dessertItem.name;
 
     button.addEventListener("click", function() {
-        if (dessert.inStock) {
-            orderSummaryContainer.textContent = `You Selected: IceCream ($3.75)`;
+        if (dessertItem.inStock) {
+            orderSummaryContainer.textContent = You Selected: ${dessertItem.name} ($${dessertItem.price.toFixed(2)});
         } else {
-            orderSummaryContainer.textContent = `Sorry, ${dessert.IceCream} is sold out.`;
-        }
-    });
-
-    menuButtonsContainer.appendChild(button);
-});
-
-dessert.forEach(dessert => {
-    const button = document.createElement("button");
-    button.textContent = dessert.FruitRoll;
-
-    button.addEventListener("click", function() {
-        if (dessert.inStock) {
-            orderSummaryContainer.textContent = `You Selected: FruitRoll ($1.50)`;
-        } else {
-            orderSummaryContainer.textContent = `Sorry, ${dessert.FruitRoll} is sold out.`;
-        }
-    });
-
-    menuButtonsContainer.appendChild(button);
-});
-
-dessert.forEach(dessert => {
-    const button = document.createElement("button");
-    button.textContent = dessert.CinnamonBun;
-
-    button.addEventListener("click", function() {
-        if (dessert.inStock) {
-            orderSummaryContainer.textContent = `You Selected: CinnamonBun ($2.25)`;
-        } else {
-            orderSummaryContainer.textContent = `Sorry, ${dessert.CinnamonBun} is sold out.`;
-        }
-    });
-
-    menuButtonsContainer.appendChild(button);
-});
-
-dessert.forEach(dessert => {
-    const button = document.createElement("button");
-    button.textContent = dessert.Cupcake;
-
-    button.addEventListener("click", function() {
-        if (dessert.inStock) {
-            orderSummaryContainer.textContent = `You Selected: Cupcake ($1.75)`;
-        } else {
-            orderSummaryContainer.textContent = `Sorry, ${dessert.Cupcake} is sold out.`;
+            orderSummaryContainer.textContent = Sorry, ${dessertItem.name} is sold out.;
         }
     });
 
